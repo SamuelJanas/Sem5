6cd119c2d275e5497033537f3a0858b59583b2a8
Samuel Janas <sneakyinsect@gmail.com>
2023-11-15 19:17:48 +0100

    working but not yet

--- Internet-Applications/scrape-that-book/app.js
+++ Internet-Applications/scrape-that-book/app.js
@@ -1,28 +1,82 @@
 const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+
 const app = express();
 const port = 3000;
-const cors = require('cors');
-
 
 app.use(cors());
 app.use(express.static('public')); // serve static files from 'public' directory
 
+let allBooks = []; // Stores all book data
+let currentIndex = 0; // Tracks the current book to scrape in detail
+
+async function fetchPage(url) {
+  try {
+    const response = await axios.get(url, {
+      responseType: 'arraybuffer'
+    });
+    const decodedContent = iconv.decode(response.data, 'utf-8'); // Decode from UTF-8
+    return decodedContent;
+  } catch (error) {
+    console.error('Error fetching the page:', error);
+    return null;
+  }
+}
+
+function extractBookLinksAndPrices(html) {
+  const $ = cheerio.load(html);
+  $('ul li .product-container').each((index, element) => {
+    const relativeLink = $(element).find('.product-image a').attr('href');
+    const fullLink = `https://www.taniaksiazka.pl${relativeLink}`;
+    const price = $(element).find('span.product-price').text().trim();
+    allBooks.push({ link: fullLink, price });
+  });
+}
+
+function extractBookDetailsFromPage(html) {
+  const $ = cheerio.load(html);
+  let bookDetails = {};
+  const title = $('div.product-info h1 span').text().trim();
+  const pagesText = $('div.product-info .book-info-line .book-info-value').text().trim();
+  const pages = parseInt(pagesText, 10);
+  bookDetails.title = title;
+  bookDetails.pages = pages;
+  return bookDetails;
+}
+
+app.get('/scrape', async (req, res) => {
+  if (currentIndex < allBooks.length) {
+    const bookBasicData = allBooks[currentIndex];
+    const bookPageContent = await fetchPage(bookBasicData.link);
+    const bookDetailedData = extractBookDetailsFromPage(bookPageContent);
+
+    const pricePerPage = bookDetailedData.pages ? (parseFloat(bookBasicData.price) / bookDetailedData.pages).toFixed(2) : 'N/A';
+    
+    const bookData = {
+      ...bookBasicData,
+      ...bookDetailedData,
+      pricePerPage
+    };
+
+    currentIndex++;
+    res.json(bookData);
+  } else {
+    res.json({ message: "No more books to scrape" });
+  }
+});
+
+async function fetchAllBookData() {
+  const url = 'https://www.taniaksiazka.pl/nowosci';
+  const pageContent = await fetchPage(url);
+  extractBookLinksAndPrices(pageContent);
+}
+
+// Fetch all book data when the server starts
+fetchAllBookData().catch(console.error);
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-app.get('/scrape', async (req, res) => {
-    const url = 'https://www.taniaksiazka.pl/bestsellery';
-    const pageContent = await fetchPage(url);
-    const books = extractBookDetails(pageContent);
-
-    if (currentIndex < books.length) {
-        const book = books[currentIndex];
-        currentIndex++;
-        res.json(book);
-      } else {
-        res.json({ message: "No more books to scrape" });
-      }
-  });
-
-  
---
+++ Internet-Applications/scrape-that-book/public/script.js
@@ -0,0 +1,47 @@
+
+function addBookToTable(book) {
+    const table = document.getElementById('booksTable').getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow();
+  
+    // Add cells for title, link, price, pages, price per page
+    const titleCell = newRow.insertCell();
+    titleCell.textContent = book.title;
+  
+    const linkCell = newRow.insertCell();
+    const link = document.createElement('a');
+    link.setAttribute('href', book.link);
+    link.textContent = 'View';
+    linkCell.appendChild(link);
+  
+    const priceCell = newRow.insertCell();
+    priceCell.textContent = book.price;
+  
+    const pagesCell = newRow.insertCell();
+    pagesCell.textContent = book.pages;
+  
+    const pricePerPageCell = newRow.insertCell();
+    pricePerPageCell.textContent = book.pricePerPage;
+
+    sortTableByPricePerPage();
+  }
+  
+function sortTableByPricePerPage() {
+    const table = document.getElementById('booksTable').getElementsByTagName('tbody')[0];
+    let rows = Array.from(table.rows);
+  
+    rows.sort((rowA, rowB) => {
+      let valueA = parseFloat(rowA.cells[4].textContent); // Assuming price per page is in the 5th column
+      let valueB = parseFloat(rowB.cells[4].textContent);
+      return valueA - valueB; // For ascending order, swap valueA and valueB for descending
+    });
+  
+    rows.forEach(row => table.appendChild(row)); // Re-adding sorted rows to the table
+  }
+
+document.getElementById('scrapeButton').addEventListener('click', async function(e) {
+    console.log("Button clicked"); // This should log once per click
+    const response = await fetch('http://localhost:3000/scrape');
+    const book = await response.json();
+    addBookToTable(book);
+  });
+  
